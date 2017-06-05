07ea5f1a95cec2bc9e4685c066528d20c2a90a47
DarkeyeGod <admin@darkeyegod.com>
2017-06-06 03:08:58 +1000

    Delete LittleMeta.js

--- LittleMeta.js
+++
@@ -1,4 +0,0 @@
-TestingFunction = function() { return "Tested."; }
-var __LittleMeta__ = {Write:"<missing>",Read:"<missing>"};
-__LittleMeta__.Write = function(META_DATA = {'Example':'Testing123'}, item = TestingFunction) { item.__Meta__ = {META_DATA}; return true; }
-__LittleMeta__.Read = function(item = TestingFunction) { if(item.__Meta__ !== undefined) { return item.__Meta__; } else { return false; } }
